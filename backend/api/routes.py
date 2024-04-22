d0fa886374da8037fe0ef843561b1d9a1c8ddcaa
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-04-22 13:28:39 +0200

    refactor: addition of basic get requests

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,12 +1,83 @@
+import logging
+import os
+
+from api.auth import init_credentials
+
+from contextlib import asynccontextmanager
+
+import numpy as np
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+
+
+resources = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # On startup
+    init_credentials()
+    resources["msg"] = "Successfully initialized credentials"
+    yield
+    # On shutdown
+    resources.clear()
+
 
 app = FastAPI(
     title="PDATAVIEWER API",
     description="API interface to access programmatic functionalities of PDATAVIEWER",
     version="0.0.0",
+    terms_of_service="https://www.scai.fraunhofer.de",
+    contact={
+        "name": "Dr. Marc Jacobs",
+        "email": "marc.jacobs@scai.fraunhofer.de",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    lifespan=lifespan,
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logger = logging.getLogger("uvicorn.info")
+
+
+@app.get("/", include_in_schema=False)
+def swagger_redirect():
+    return RedirectResponse(url="/docs")
+
+
+@app.get("/version", tags=["info"])
+def get_current_version():
+    app.version
+
+
+@app.get("/error", status_code=200, tags=["info"])
+def get_error_log(response: Response):
+    if os.path.isfile("error.txt"):
+        with open("error.txt") as f:
+            error = f.read()
+        return {"content": error}
+    else:
+        response.status_code = 204
+        return {"content": "No error occurred."}
+
+@app.get("/datasets", tags=["info"])
+def get_available_cdms():
+    datasets = [f for f in os.listdir("data") if os.path.isdir(os.path.join("data", f))]
+    datasets_sorted = np.sort(datasets)
+    datasets_sorted_without_default = np.delete(datasets_sorted, np.argwhere(datasets_sorted == "default"))
+    return datasets_sorted_without_default.tolist()
