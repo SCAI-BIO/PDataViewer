d79acd6b7e307287dcf3da395e4585b07439a476
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-07 11:04:13 +0200

    refactor: change the default page to home

--- pdataviewer/src/app/app.routes.ts
+++ pdataviewer/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: HomeComponent
+    }
+];
