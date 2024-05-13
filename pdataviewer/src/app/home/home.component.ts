1acb1c110acc03c3b24801b5a2c8cc11470c1a66
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-13 11:42:49 +0200

    add: navigation bar

--- pdataviewer/src/app/home/home.component.ts
+++ pdataviewer/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
