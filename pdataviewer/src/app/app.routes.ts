c3ccb447312ddb09334b4153f43295ce565caf8f
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-13 11:41:38 +0200

    add: study-picker to path

--- pdataviewer/src/app/app.routes.ts
+++ pdataviewer/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StudyPickerComponent } from './study-picker/study-picker.component';
 
 export const routes: Routes = [
-    {
-        path: "", component: HomeComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'study-picker',
+    component: StudyPickerComponent,
+  },
 ];
