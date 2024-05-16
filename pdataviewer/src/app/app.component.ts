a91b6ee250f2c968032e6ec8d7969e661e268c73
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-16 14:45:55 +0200

    delete: empty space

--- pdataviewer/src/app/app.component.ts
+++ pdataviewer/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
