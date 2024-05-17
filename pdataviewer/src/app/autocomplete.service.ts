cad72c41eafdd88612aff3e1bd440cf88c0b9b89
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-17 14:40:11 +0200

    add: documentation and error handling

--- pdataviewer/src/app/autocomplete.service.ts
+++ pdataviewer/src/app/autocomplete.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutocompleteService {
+  private apiUrl = 'http://127.0.0.1:5000'; // Base URL
+  private autocompleteEndpoint = '/autocompletion'; // Endpoint
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
+  /**
+   * Fetches autocomplete suggestions based on the input text.
+   * @param input The input text for autocomplete.
+   * @returns An Observable emitting an array of autocomplete suggestions.
+   */
   autocomplete(input: string): Observable<string[]> {
-    return this.http.get<string[]>('http://127.0.0.1:5000/autocompletion?text=' + input);
+    const params = new HttpParams().set('text', input);
+    const url = `${this.apiUrl}${this.autocompleteEndpoint}`;
+    return this.http.get<string[]>(url, { params }).pipe(
+      catchError((error) => {
+        // Handle the error here
+        console.error('An error occurred:', error);
+        return throwError(() => error);
+      })
+    );
   }
 }
