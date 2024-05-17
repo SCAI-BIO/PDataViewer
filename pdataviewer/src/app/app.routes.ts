bdbde28540421f5482beaa7e49f6242995d36ecd
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-17 14:55:55 +0200

    add: mappings component

--- pdataviewer/src/app/app.routes.ts
+++ pdataviewer/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StudyPickerComponent } from './study-picker/study-picker.component';
+import { MappingsComponent } from './mappings/mappings.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'study-picker',
     component: StudyPickerComponent,
+  },
+  {
+    path: 'mappings',
+    component: MappingsComponent,
   },
 ];
