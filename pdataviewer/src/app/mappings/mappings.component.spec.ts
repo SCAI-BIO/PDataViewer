b3b5335b65a3cf16bea7457e6284c710dbc9b352
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-17 14:55:44 +0200

    add: mappings component

---
+++ pdataviewer/src/app/mappings/mappings.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MappingsComponent } from './mappings.component';
+
+describe('MappingsComponent', () => {
+  let component: MappingsComponent;
+  let fixture: ComponentFixture<MappingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MappingsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MappingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ pdataviewer/src/app/mappings/mappings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-mappings',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './mappings.component.html',
+  styleUrl: './mappings.component.css'
+})
+export class MappingsComponent {
+
+}
