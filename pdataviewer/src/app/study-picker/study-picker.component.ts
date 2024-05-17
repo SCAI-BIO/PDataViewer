504e76ef8230cc2ad591776dfc098f3a376555b0
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-17 14:40:42 +0200

    refactor: alphabetically reorder variables

--- pdataviewer/src/app/study-picker/study-picker.component.ts
+++ pdataviewer/src/app/study-picker/study-picker.component.ts
@@ -31,42 +31,30 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
   styleUrl: './study-picker.component.css',
 })
 export class StudyPickerComponent implements OnInit, OnDestroy {
-  private apiUrl = 'http://127.0.0.1:5000/';
-  private subscriptions: Subscription[] = [];
-  // Form control for the feature input.
-  featureCtrl = new FormControl();
   // Array of cohort rankings.
   cohortRankings: any = [];
-  // Observable for feature suggestions.
-  suggestions$: Observable<string[]> | null = null;
+  // Form control for the feature input.
+  featureCtrl = new FormControl();
   // Array of available features.
   features: string[] = [];
   // Observable for filtered features.
   filteredFeatures: Observable<string[]> | null = null;
   // Array of features selected by the user
   @Input() selectedFeatures: string[] = [];
+  // Observable for feature suggestions.
+  suggestions$: Observable<string[]> | null = null;
+  private apiUrl = 'http://127.0.0.1:5000/';
+  private subscriptions: Subscription[] = [];
 
   constructor(private http: HttpClient) {}
-
-  // Fetches features and initializes features and filteredFeatures when the component is initialized.
-  ngOnInit() {
-    const sub = this.fetchFeatures().subscribe((features) => {
-      this.features = features.Feature;
-      this.filteredFeatures = this.featureCtrl.valueChanges.pipe(
-        startWith(''),
-        map((value) => this._filter(value))
-      );
-    });
-    this.subscriptions.push(sub);
-  }
 
   /**
    * Adds a feature to the selected features list.
    * @param event - The MatChipInputEvent object.
    */
   addFeature(event: MatChipInputEvent): void {
-    const input = event.chipInput;
     let feature = event.value;
+    const input = event.chipInput;
 
     feature = (feature || '').trim();
     if (feature && !this.selectedFeatures.includes(feature)) {
@@ -79,25 +67,6 @@ export class StudyPickerComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * Removes a feature from the selected features list.
-   * @param feature - The feature to remove.
-   */
-  removeFeature(feature: string): void {
-    const index = this.selectedFeatures.indexOf(feature);
-    if (index >= 0) {
-      this.selectedFeatures.splice(index, 1);
-    }
-  }
-
-  /**
-   * Fetches the available features from the API.
-   * @returns An Observable of the fetched features.
-   */
-  fetchFeatures(): Observable<{ Feature: string[] }> {
-    return this.http.get<{ Feature: string[] }>(this.apiUrl + 'cdm/features');
-  }
-
-  /**
    * Displays the feature name in the autocomplete input.
    * @param feature - The feature to display.
    * @returns The feature name or an empty string.
@@ -107,27 +76,11 @@ export class StudyPickerComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * Filters the features based on the input value.
-   * @param value - The input value to filter by.
-   * @returns An array of filtered features.
+   * Fetches the available features from the API.
+   * @returns An Observable of the fetched features.
    */
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.features.filter((feature) =>
-      feature.toLowerCase().includes(filterValue)
-    );
-  }
-
-  /**
-   * Handles the selection of a feature from the autocomplete dropdown.
-   * @param event - The MatAutocompleteSelectedEvent object.
-   */
-  selected(event: MatAutocompleteSelectedEvent): void {
-    const feature = event.option.value;
-    if (feature && !this.selectedFeatures.includes(feature)) {
-      this.selectedFeatures.push(feature);
-      this.featureCtrl.setValue('');
-    }
+  fetchFeatures(): Observable<{ Feature: string[] }> {
+    return this.http.get<{ Feature: string[] }>(this.apiUrl + 'cdm/features');
   }
 
   /**
@@ -145,8 +98,55 @@ export class StudyPickerComponent implements OnInit, OnDestroy {
     this.subscriptions.push(sub);
   }
 
+  // Fetches features and initializes features and filteredFeatures when the component is initialized.
+  ngOnInit() {
+    const sub = this.fetchFeatures().subscribe((features) => {
+      this.features = features.Feature;
+      this.filteredFeatures = this.featureCtrl.valueChanges.pipe(
+        startWith(''),
+        map((value) => this._filter(value))
+      );
+    });
+    this.subscriptions.push(sub);
+  }
+
   // Unsubscribes from subscriptions when the component is destroyed to prevent memory leaks.
   ngOnDestroy(): void {
     this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  /**
+   * Handles the selection of a feature from the autocomplete dropdown.
+   * @param event - The MatAutocompleteSelectedEvent object.
+   */
+  optionSelected(event: MatAutocompleteSelectedEvent): void {
+    const feature = event.option.value;
+    if (feature && !this.selectedFeatures.includes(feature)) {
+      this.selectedFeatures.push(feature);
+      this.featureCtrl.setValue('');
+    }
+  }
+
+  /**
+   * Removes a feature from the selected features list.
+   * @param feature - The feature to remove.
+   */
+  removeFeature(feature: string): void {
+    const index = this.selectedFeatures.indexOf(feature);
+    if (index >= 0) {
+      this.selectedFeatures.splice(index, 1);
+    }
+  }
+
+  /**
+   * Filters the features based on the input value.
+   * @param value - The input value to filter by.
+   * @returns An array of filtered features.
+   */
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.features.filter((feature) =>
+      feature.toLowerCase().includes(filterValue)
+    );
   }
 }
