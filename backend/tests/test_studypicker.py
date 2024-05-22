75dc9355c91f75feb30d0c46b87152b3495d2edc
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-22 08:59:11 +0200

    refactor: column names

--- backend/tests/test_studypicker.py
+++ backend/tests/test_studypicker.py
@@ -6,17 +6,17 @@ def test_rank_cohorts():
     ranked_cohorts = rank_cohorts(features=features, folder="./backend/tests/resources")
 
     rank_cohorts_result = {
-        "Cohort (ranked)": {0: "cohort_x", 1: "cohort_y", 2: "cohort_z"},
-        "Successfully found": {0: "2/3 (66.67%)", 1: "2/3 (66.67%)", 2: "1/3 (33.33%)"},
-        "Missing features": {0: "bmi", 1: "bmi", 2: "age, bmi"},
+        "cohort": {0: "cohort_x", 1: "cohort_y", 2: "cohort_z"},
+        "found": {0: "2/3 (66.67%)", 1: "2/3 (66.67%)", 2: "1/3 (33.33%)"},
+        "missing": {0: "bmi", 1: "bmi", 2: "age, bmi"},
     }
 
     # Assert the shape of the DataFrame
     assert ranked_cohorts.shape == (3, 3)
     # Assert the column data types
-    assert ranked_cohorts["Cohort (ranked)"].dtype == "object"
-    assert ranked_cohorts["Successfully found"].dtype == "object"
-    assert ranked_cohorts["Missing features"].dtype == "object"
+    assert ranked_cohorts["cohort"].dtype == "object"
+    assert ranked_cohorts["found"].dtype == "object"
+    assert ranked_cohorts["missing"].dtype == "object"
     # Assert the values in the DataFrame
     assert rank_cohorts_result == ranked_cohorts.to_dict()
     # Check if the DataFrame is empty when no cohorts are found
