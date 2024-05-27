fec6ddc59b7d71b6b2fe904f1e4bcd4c0df67830
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-27 10:36:26 +0200

    remove: commented code

--- pdataviewer/server.ts
+++ pdataviewer/server.ts
@@ -17,9 +17,6 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
