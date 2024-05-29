c9316459e495724092519286526da324dc09561f
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-29 11:37:58 +0200

    refactor: change of function logic

--- backend/functions/visualization.py
+++ backend/functions/visualization.py
@@ -3,25 +3,26 @@ import pandas as pd
 
 
 def generate_chords(modality: str, cohorts: list[str], folder: str='./cdm'):
-    """Generate linkage information for cohorts in a modality.
+    """Generate linkage information for cohorts in a specified modality.
 
-    The variables of each cohort will be encoded in numbers consecutively
-    starting from 1. A modality specific decoder can be found in
-    datasets/decoder in .json format.
+    This function reads a CSV file corresponding to the given modality from the specified folder,
+    processes the data to filter out rows and columns based on the presence of mappings,
+    and constructs linkage information in the form of nodes and links between the cohorts.
 
     Args:
-        modality (str): Name of the modality
-        cohorts (list[str]): Cohorts to be included in the mappings
-        folder (str, optional): Path to the folder containing the modality
+        modality (str): Name of the modality, used to identify the CSV file in the folder.
+        cohorts (list[str]): List of cohort names to be included in the mappings.
+        folder (str, optional): Path to the folder containing the modality CSV file. Defaults to './cdm'.
 
     Raises:
-        FileNotFoundError: The folder does not exist
-        FileNotFoundError: The folder is empty
-        ValueError: cohorts list cannot be empty
+        FileNotFoundError: If the specified folder does not exist.
+        FileNotFoundError: If the specified folder is empty.
+        ValueError: If the cohorts list is empty
 
     Returns:
-        chords (dict[Hashable, Any]): A dictionary containing linkage information
-        decoder (dict[Hashable, Any]): A dictionary to decode the numbers in the linkage information
+        dict: A dictionary containing:
+            - 'nodes': A list of dictionaries, each representing a node with 'name' and 'group'.
+            - 'links': A list of dictionaries, each representing a link with 'source' and 'target'.
     """
     # Check if the folder exists
     if not os.path.exists(folder):
@@ -31,38 +32,37 @@ def generate_chords(modality: str, cohorts: list[str], folder: str='./cdm'):
         raise FileNotFoundError(f"the folder '{folder}' is empty.")
     # Check if the cohorts list is empty
     if not cohorts:
-        raise ValueError("The 'cohorts' list cannot be empty.")    
-    # Initialize a dictionary to decode the numbers of variables later on, and save used cohorts
-    decoder = {}
-    decoder["cohorts"] = cohorts
+        raise ValueError("The 'cohorts' list cannot be empty.")
+    
+    data = {}
+    nodes = []
+    links = []
+
     # Read the .csv file
     mappings = pd.read_csv(f"{folder}/{modality}.csv", usecols=cohorts)
     # Filter out rows with only 1 mapping
     mappings = mappings[mappings.notna().sum(axis=1) > 1]
     # Filter out empty columns
     mappings.dropna(how="all", axis=1, inplace=True)
-    # Encode DataFrame values to consecutive numbers (starting from 1) for non-NaN values
+    # For comma separated multiple mappings, create a new row for each mapping
     for column in mappings.columns:
-        number = 1
-        for row in mappings.index:
-            if pd.notna(mappings.loc[row, column]):
-                # If the cohort does not exist in the dictionary,
-                # initialize it with an empty dictionary
-                if column not in decoder.keys():
-                    decoder[column] = {}
-                # Save the encoded number of the variable to decoder dictionary
-                decoder[column][number] = mappings.loc[row, column]
-                mappings.loc[row, column] = number
-                number += 1
-    # Create an empty data frame with column names
-    chords = pd.DataFrame(columns=["link_id", "cohort", "start", "end"])
-    # Populate chords with link_ids
-    link_id = 0
-    for row in mappings.index:
-        for column in mappings.columns:
-            value = mappings.loc[row, column]
-            if pd.notna(value):
-                chords.loc[len(chords.index)] = [f"link_{link_id}", column, value, value]
-        link_id += 1
-    chords = chords.to_dict()
-    return chords, decoder
+        if mappings[column].apply(lambda x: isinstance(x, str) and ', ' in x).any():
+            mappings[column] = mappings[column].str.split(', ')
+            mappings = mappings.explode(column)
+    filtered_cohorts = mappings.columns
+
+    for _, row in mappings.iterrows():
+        for i in range(len(filtered_cohorts)):
+            source = row[filtered_cohorts[i]]
+            for j in range(i + 1, len(filtered_cohorts)):
+                target = row[filtered_cohorts[j]]
+                if pd.notna(target) and pd.notna(source):
+                    if {"name": source, "group": filtered_cohorts[i]} not in nodes:
+                        nodes.append({"name": source, "group": filtered_cohorts[i]})
+                    if {"name": target, "group": filtered_cohorts[j]} not in nodes:
+                        nodes.append({"name": target, "group": filtered_cohorts[j]})
+                    links.append({"source": source, "target": target})
+
+    data["links"] = links
+    data["nodes"] = list(nodes)
+    return data
