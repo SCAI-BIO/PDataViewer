32b6c6c1c22e646721fee7d30453168364e347a3
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-31 11:37:46 +0200

    style: alphabetical reordering

--- pdataviewer/src/app/mappings/mappings.component.ts
+++ pdataviewer/src/app/mappings/mappings.component.ts
@@ -17,6 +17,10 @@ import { ChordDiagramService } from '../services/chord-diagram.service';
   styleUrl: './mappings.component.css',
 })
 export class MappingsComponent implements OnInit, OnDestroy {
+  // Array to hold chunks of data for creating chord diagrams
+  public dataChunks: any[] = [];
+  // Array to hold available modalities
+  public modalities: string[] = [];
   // API_URL for fetching data from the backend
   private API_URL = environment.API_URL;
   // Array to hold cohort names
@@ -25,23 +29,13 @@ export class MappingsComponent implements OnInit, OnDestroy {
   private modality: string = '';
   // Array to hold subscriptions to be cleaned up on destroy
   private subscriptions: Subscription[] = [];
-  // Array to hold available modalities
-  public modalities: string[] = [];
-  // Array to hold chunks of data for creating chord diagrams
-  public dataChunks: any[] = [];
 
   // Inject HttpClient for making HTTP requests
   // Inject ChordDiagramService for handling chord diagram creation
   constructor(
-    private http: HttpClient,
-    private chordService: ChordDiagramService
+    private chordService: ChordDiagramService,
+    private http: HttpClient
   ) {}
-
-  // Lifecycle hook that runs when the component is initialized
-  ngOnInit(): void {
-    this.fetchModalities(); // Fetch the available modalities
-    this.fetchCohorts(); // Fetch the available cohorts
-  }
 
   // Lifecycle hook that runs when the component is destroyed
   ngOnDestroy(): void {
@@ -49,24 +43,16 @@ export class MappingsComponent implements OnInit, OnDestroy {
     this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
+  // Lifecycle hook that runs when the component is initialized
+  ngOnInit(): void {
+    this.fetchModalities(); // Fetch the available modalities
+    this.fetchCohorts(); // Fetch the available cohorts
+  }
+
   // Handler for modality click events
   onModalityClick(modality: string): void {
     this.modality = modality; // Set the current modality
     this.fetchData(); // Fetch data for the selected modality
-  }
-
-  // Fetch the available modalities from the backend
-  private fetchModalities(): void {
-    const sub = this.http
-      .get<string[]>(`${this.API_URL}/cdm/modalities`)
-      .subscribe({
-        next: (v) => {
-          this.modalities = v;
-        },
-        error: (e) => console.error('Error fetching the modalities:', e),
-        complete: () => console.info('Modalities fetched successfully.'),
-      });
-    this.subscriptions.push(sub);
   }
 
   // Fetch the available cohorts from the backend
@@ -99,6 +85,20 @@ export class MappingsComponent implements OnInit, OnDestroy {
         error: (e) => console.error('Error fetching chord data:', e),
         complete: () =>
           console.info('Chord diagram data fetched successfully.'),
+      });
+    this.subscriptions.push(sub);
+  }
+
+  // Fetch the available modalities from the backend
+  private fetchModalities(): void {
+    const sub = this.http
+      .get<string[]>(`${this.API_URL}/cdm/modalities`)
+      .subscribe({
+        next: (v) => {
+          this.modalities = v;
+        },
+        error: (e) => console.error('Error fetching the modalities:', e),
+        complete: () => console.info('Modalities fetched successfully.'),
       });
     this.subscriptions.push(sub);
   }
