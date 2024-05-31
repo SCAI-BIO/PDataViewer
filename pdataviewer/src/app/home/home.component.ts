4587abf3ff29eaac38cb8531b5dc3ca8458f30ac
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-05-31 22:18:13 +0200

    update: home component

--- pdataviewer/src/app/home/home.component.ts
+++ pdataviewer/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -6,7 +7,8 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, NavBarComponent],
+  imports: [FooterComponent, NavBarComponent, RouterModule],
   templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {}
