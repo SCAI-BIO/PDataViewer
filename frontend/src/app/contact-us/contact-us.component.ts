a21bd7a5fe2954dd6c0bf760449c850d398e42e2
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-06-11 10:17:04 +0200

    refactor: change styles

--- frontend/src/app/contact-us/contact-us.component.ts
+++ frontend/src/app/contact-us/contact-us.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css',
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'x',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/social-media/x.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'orcid',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/social-media/orcid.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'linkedin',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/social-media/linkedin.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'google-scholar',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/social-media/google-scholar.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'stack-overflow',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'assets/social-media/stack-overflow.svg'
+      )
+    );
+  }
+}
