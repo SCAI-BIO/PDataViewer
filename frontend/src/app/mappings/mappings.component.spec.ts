989d26bcd4218f2affc8e8359cb2e1fa13d4507e
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-06-11 11:57:00 +0200

    refactor: style change

--- frontend/src/app/mappings/mappings.component.spec.ts
+++ frontend/src/app/mappings/mappings.component.spec.ts
@@ -1,8 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
 import { By } from '@angular/platform-browser';
 
