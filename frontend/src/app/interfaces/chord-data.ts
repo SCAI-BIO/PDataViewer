dfc9ca81353d6972271f617840287ddb62323274
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-06-17 13:44:21 +0200

    add: ChordData interface

---
+++ frontend/src/app/interfaces/chord-data.ts
@@ -0,0 +1,8 @@
+import { ChordNode } from "../interfaces/chord-node"
+import { ChordLink } from "./chord-link"
+
+export interface ChordData {
+    nodes: ChordNode[];
+    links: ChordLink[];
+  }
+  
