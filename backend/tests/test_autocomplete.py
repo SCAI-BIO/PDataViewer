d9a5e4e4b1f639c9b597068eda78561815bc196f
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-06-28 13:59:49 +0200

    refactor: make autocomplete tests independent from the actual database

--- backend/tests/test_autocomplete.py
+++ backend/tests/test_autocomplete.py
@@ -1,48 +1,75 @@
+from unittest.mock import MagicMock
+
+import pandas as pd
+import pytest
 from functions.autocomplete import autocomplete
 
 
-def test_autocomplete_matching_prefix():
-    query = "ag"
-    suggestions = autocomplete(query, path="backend/db/cdm.db")
-    result = [
-        "Age",
-        "Aging-related tau Astrogliopathy (ARTAG) Severity",
-        "Diagnosis",
-        "IDEA - Leader of the Village",
-        "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) A Score",
-        "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) B Score",
-        "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) C Score",
-        "MDS-UPDRS - Right Leg Agility",
-        "MDS-UPDRS - Left Leg Agility",
-        "MDS-UPDRS - Hoehn and Yahr Stage",
-    ]
+@pytest.fixture
+def mock_cdm_repository():
+    mock_repo = MagicMock()
+    mock_repo.get_columns.return_value = pd.DataFrame(
+        {
+            "Feature": [
+                "Age",
+                "Aging-related tau Astrogliopathy (ARTAG) Severity",
+                "Diagnosis",
+                "IDEA - Leader of the Village",
+                "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) A Score",
+                "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) B Score",
+                "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) C Score",
+                "MDS-UPDRS - Right Leg Agility",
+                "MDS-UPDRS - Left Leg Agility",
+                "MDS-UPDRS - Hoehn and Yahr Stage",
+            ]
+        }
+    )
+    return mock_repo
+
+
+@pytest.mark.parametrize(
+    "query, expected_result",
+    [
+        (
+            "ag",
+            [
+                "Age",
+                "Aging-related tau Astrogliopathy (ARTAG) Severity",
+                "Diagnosis",
+                "IDEA - Leader of the Village",
+                "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) A Score",
+                "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) B Score",
+                "The National Institute on Aging in collaboration with the Alzheimer's Association (NIA-AA) C Score",
+                "MDS-UPDRS - Right Leg Agility",
+                "MDS-UPDRS - Left Leg Agility",
+                "MDS-UPDRS - Hoehn and Yahr Stage",
+            ],
+        ),
+        (
+            "agy",
+            [
+                "Age",
+                "Aging-related tau Astrogliopathy (ARTAG) Severity",
+                "MDS-UPDRS - Right Leg Agility",
+                "MDS-UPDRS - Left Leg Agility",
+            ],
+        ),
+        ("xsdsxa", []),
+    ],
+)
+def test_autocomplete(mock_cdm_repository, query, expected_result):
+    suggestions = autocomplete(query, repo=mock_cdm_repository)
     assert len(suggestions) <= 10
-    assert suggestions == result
+    assert suggestions == expected_result
 
 
-def test_autocomplete_typo():
-    query = "agy"
-    suggestions = autocomplete(query, path="backend/db/cdm.db")
-    result = [
-        "Aging-related tau Astrogliopathy (ARTAG) Severity",
-        "MDS-UPDRS - Right Leg Agility",
-        "MDS-UPDRS - Left Leg Agility",
-        "Age",
-    ]
-    assert len(suggestions) <= 10
-    assert suggestions == result
-
-
-def test_autocomplete_meaningless_query():
-    query = "xsdsxa"
-    suggestions = autocomplete(query, path="backend/db/cdm.db")
-    result = []
-    assert suggestions == result
-
-
-def test_autocomplete_remove_special_characters():
-    query1 = "ag\s\s"
-    query2 = "agss"
-    suggestions1 = autocomplete(query1, path="backend/db/cdm.db")
-    suggestions2 = autocomplete(query2, path="backend/db/cdm.db")
+@pytest.mark.parametrize(
+    "query1, query2",
+    [
+        ("ag\\s\\s", "agss"),
+    ],
+)
+def test_autocomplete_remove_special_characters(mock_cdm_repository, query1, query2):
+    suggestions1 = autocomplete(query1, repo=mock_cdm_repository)
+    suggestions2 = autocomplete(query2, repo=mock_cdm_repository)
     assert suggestions1 == suggestions2
