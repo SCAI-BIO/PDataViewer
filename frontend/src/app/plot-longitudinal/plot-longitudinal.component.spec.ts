8d971e6b9713e730f1b9c829610e1e8fab50eb3e
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-07-16 13:05:11 +0200

    refactor: add activated route

--- frontend/src/app/plot-longitudinal/plot-longitudinal.component.spec.ts
+++ frontend/src/app/plot-longitudinal/plot-longitudinal.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { PlotLongitudinalComponent } from './plot-longitudinal.component';
 
 describe('PlotLongitudinalComponent', () => {
@@ -8,10 +10,20 @@ describe('PlotLongitudinalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlotLongitudinalComponent]
-    })
-    .compileComponents();
-    
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              cohort: 'testCohort',
+              features: ['feature1', 'feature2'],
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PlotLongitudinalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
