6d8fdd4379c2b66ee0bea9d9054da091c9f63e80
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2024-07-17 15:55:56 +0200

    add: password hashing

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,4 +1,5 @@
 import os
+from hashlib import sha256
 
 from dotenv import load_dotenv
 from fastapi import Depends, HTTPException
@@ -6,19 +7,20 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 security = HTTPBasic()
 
+
 def init_credentials():
     """Initializes admin credentials from environment variables or a .env file.
-    
+
     This function checks for the presence of the `PDATAVIEWER_ADMIN_USERNAME`and
     `PDATAVIEWER_ADMIN_PASSWORD`environment variables. If these variables are not found,
-    it loads them from a .env file. After loading, it checks for the presence of the 
+    it loads them from a .env file. After loading, it checks for the presence of the
     `PDATAVIEWER_ADMIN_USERNAME`and`PDATAVIEWER_ADMIN_PASSWORD`environment variables again.
     If the credentials are still missing after attempting to load from the .env file, an HTTP
     exception is raised.
 
     Raises:
         HTTPException: If the admin credentials are missing after attempting to load from the environment and .env file, an HTTP 500 Internal Server Error exception is raised.
-    
+
     Returns:
         bool: Returns True if the admin credentials are successfully initialized.
     """
@@ -26,11 +28,14 @@ def init_credentials():
     admin_password = os.getenv("PDATAVIEWER_ADMIN_PASSWORD")
     if not admin_username or not admin_password:
         load_dotenv()
-    if os.getenv("PDATAVIEWER_ADMIN_USERNAME") and os.getenv("PDATAVIEWER_ADMIN_PASSWORD"):
+    if os.getenv("PDATAVIEWER_ADMIN_USERNAME") and os.getenv(
+        "PDATAVIEWER_ADMIN_PASSWORD"
+    ):
         return True
     else:
         raise HTTPException(status_code=500, detail="Missing admin credentials")
-    
+
+
 def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
     """Authenticates a user based on the provided credentials.
 
@@ -46,10 +51,11 @@ def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
 
     Returns:
         bool: Returns True if the authentication is successful.
-    """    
+    """
     username = os.getenv("PDATAVIEWER_ADMIN_USERNAME")
     password = os.getenv("PDATAVIEWER_ADMIN_PASSWORD")
-    if credentials.username != username or credentials.password != password:
+    hashed_password = sha256(credentials.password.encode("utf-8")).hexdigest()
+    if credentials.username != username or hashed_password != password:
         raise HTTPException(status_code=401, detail="Incorrect credentials")
     else:
         return True
