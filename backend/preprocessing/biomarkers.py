6e0fe1f07e625b495493051db5b0e1b5954c6b85
Mehmet Can Ay <mehmet.ay@scai.fraunhofer.de>
2024-09-06 13:07:29 +0200

    fix: biomarker processing code

--- backend/preprocessing/biomarkers.py
+++ backend/preprocessing/biomarkers.py
@@ -12,7 +12,7 @@ def extract_features(
 
     Args:
         df_dict (dict[str, pd.DataFrame]): Dictionary of data frames containing cohort data.
-        feature_df (pd.DataFrame): Data frame of mapped features.
+        mapping_df (pd.DataFrame): Data frame of mapped features.
 
     Returns:
         result_dict (dict[str, dict[str, list[dict[str, int | float | str]]]]): A nested dictionary.
@@ -23,29 +23,29 @@ def extract_features(
     result_dict = {}
 
     for feature in mapping_df.Feature:
+
+        # Extract only feature column from PASSIONATE
         feature_row = mapping_df.loc[mapping_df["Feature"] == feature]
 
         for cohort in mapping_df.columns.intersection(list(df_dict.keys())):
             feat = feature_row[cohort].item()
 
-            if feat == 0:
+            # If the mapping is empty continue
+            if pd.isna(feat):
                 continue
 
+            # If the feature mapped to more than one term, take the first one
             if ", " in feat:
-                feat = feat.split(", ")[1]
+                feat = feat.split(", ")[0]
 
-            target_cols = [col for col in df_dict[cohort].columns if feat in col]
-
-            if not target_cols:
+            # The mapped feature might not contain valid measurements
+            # In this case drop the column
+            if feat not in df_dict[cohort].columns:
                 continue
 
-            exact_match = next((col for col in target_cols if col == feat), None)
-            selected_col = exact_match if exact_match else target_cols[0]
-
-            # Filter rows where both measurement and diagnosis are valid
+            # Filter rows where both the Measurement and the Diagnosis contain valid information.
             valid_rows = df_dict[cohort][
-                (df_dict[cohort][selected_col] != 0)
-                & (df_dict[cohort]["Diagnosis"] != 0)
+                (df_dict[cohort][feat].notna()) & (df_dict[cohort]["Diagnosis"].notna())
             ]
 
             if not valid_rows.empty:
@@ -57,7 +57,7 @@ def extract_features(
                 for _, row in valid_rows.iterrows():
                     result_dict[feature][cohort].append(
                         {
-                            "Measurement": row[selected_col],
+                            "Measurement": row[feat],
                             "Diagnosis": row["Diagnosis"],
                         }
                     )
@@ -70,13 +70,11 @@ base_path = Path("backend/data")
 
 ### READ CDM MODALITIES ###
 cdm_files = sorted(base_path.glob("cdm/*.csv"))
-modalities = [file.stem for file in cdm_files]
-dataframes = [pd.read_csv(file) for file in cdm_files]
+modalities = [file.stem for file in cdm_files]  # names of the data frames
+dataframes = [pd.read_csv(file) for file in cdm_files]  # the data frames
 
 # Create a dictionary with all modalities as dataframes
-all_features = {
-    modality.split(" - ")[1]: df for modality, df in zip(modalities, dataframes)
-}
+all_features = {modality.lower(): df for modality, df in zip(modalities, dataframes)}
 
 # Drop irrelevant columns
 for df in all_features.values():
@@ -88,47 +86,35 @@ merged_df = pd.concat(all_features.values(), ignore_index=True)
 # Replace "No total score." as the test was performed but the total score was not reported
 merged_df.replace({"No total score.": np.nan}, inplace=True)
 
-# Fill all the NaN cells with 0
-merged_df.fillna(0, inplace=True)
-
 # Filter numeric features
 numeric_features = merged_df.loc[merged_df.Rank == 2]
 
 ### READ PATIENT LEVEL DATA ###
 patient_level_files = sorted(base_path.glob("patient_level/*.csv"))
-cohorts = [file.stem for file in patient_level_files]
+cohorts = [file.stem for file in patient_level_files]  # names of the cohorts
 datasets = [
-    pd.read_csv(file, index_col=0, low_memory=False) for file in patient_level_files
+    pd.read_csv(file, index_col=0, low_memory=False)
+    for file in patient_level_files  # the actual dataframes
 ]
 # Create a dictionary with all cohorts as dataframes
 cohort_studies = {
-    cohort: df.loc[(df["Months"] == 0)] for cohort, df in zip(cohorts, datasets)
+    cohort: df.loc[(df["Months"] == 0)]
+    for cohort, df in zip(cohorts, datasets)  # Only utilizing baseline visit
 }
 
-# Make the index column consistent among the cohort dataframes
-for cohort, df in cohort_studies.items():
-    df["ID"] = df.index
-    cohort_studies[cohort] = df.reset_index(drop=True).set_index("ID")
+# Drop empty columns from the dataframes
+for cohort, _ in cohort_studies.items():
+    # Please make the patient ID column naming consistent
+    # and set it as the index
     cohort_studies[cohort].dropna(axis=1, how="all", inplace=True)
 
 result = extract_features(cohort_studies, numeric_features)
 
 # Convert each feature dictionary into a dataframe and save it as csv file
-output_path = base_path / "processed"
+output_path = base_path / "processed/biomarker"
 output_path.mkdir(parents=True, exist_ok=True)
 
 for feature, feature_data in result.items():
-    if feature in {"Age", "Education"}:
-        for cohort in feature_data:
-            feature_data[cohort] = [
-                {
-                    "Measurement": (
-                        int(d["Measurement"]) if not pd.isna(d["Measurement"]) else 0
-                    ),
-                    "Diagnosis": d["Diagnosis"],
-                }
-                for d in feature_data[cohort]
-            ]
 
     # Create a list to store the data
     data = []
