6bba81ecaec9d2f140a611a5b7eaeff8312596d5
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-25 11:38:46 +0200

    refactor: reorder imports in FooterComponent for consistency

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule, MatIconModule, MatListModule],
+  imports: [MatIconModule, MatListModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
