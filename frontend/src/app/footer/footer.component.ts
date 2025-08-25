904abfe40dcead45b0444126ffebab60c23ec8df
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-25 16:03:55 +0200

    refactor: remove unused MatIconModule and MatMenuModule imports from footer and nav-bar components

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatIconModule, MatListModule, RouterModule],
+  imports: [MatListModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatButtonModule, MatMenuModule, RouterModule],
+  imports: [MatButtonModule, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
