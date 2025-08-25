8989eeaecd08caf6af665fc7c8f1836fa6198743
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-25 12:49:07 +0200

    feat: refactor data fetching methods in MappingsComponent to use ApiService and improve error handling

--- frontend/src/app/mappings/mappings.component.ts
+++ frontend/src/app/mappings/mappings.component.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSliderModule } from '@angular/material/slider';
@@ -8,8 +7,8 @@ import { Subscription, Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { ChordData } from '../interfaces/chord';
+import { ApiService } from '../services/api.service';
 import { ChordDiagramService } from '../services/chord-diagram.service';
-import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-mappings',
@@ -18,24 +17,123 @@ import { environment } from '../../environments/environment';
   styleUrl: './mappings.component.scss',
 })
 export class MappingsComponent implements OnInit, OnDestroy {
+  cohorts: string[] = [];
   dataChunks: ChordData[] = [];
   loading = false;
   // Maximum amount of features to display in a single chords diagram
   maxFeatures = 50;
   // Minimum amount of features for the min variable of the slider
   minFeatures = 20;
+  modality = '';
   modalities: string[] = [];
   noData = false;
   selectedModality = '';
   // Total number of features in the modality
   totalFeatures = 0;
-  private API_URL = environment.API_URL;
-  private cohorts: string[] = [];
+  private apiService = inject(ApiService);
   private chordService = inject(ChordDiagramService);
-  private http = inject(HttpClient);
-  private modality = '';
   private subscriptions: Subscription[] = [];
   private sliderChange$: Subject<number> = new Subject<number>();
+
+  fetchCohorts(): void {
+    this.loading = true;
+    const sub = this.apiService.fetchCohorts().subscribe({
+      next: (v) => {
+        this.cohorts = v;
+      },
+      error: (err) => {
+        console.error('Error fetching cohorts', err);
+        this.loading = false;
+        const detail = err.error?.detail;
+        const message = err.error?.message || err.message;
+
+        let errorMessage = 'An unknown error occurred.';
+        if (detail && message) {
+          errorMessage = `${message} — ${detail}`;
+        } else if (detail || message) {
+          errorMessage = detail || message;
+        }
+
+        alert(`An error occurred while fetching cohorts: ${errorMessage}`);
+      },
+      complete: () => (this.loading = false),
+    });
+    this.subscriptions.push(sub);
+  }
+
+  fetchData(): void {
+    this.loading = true;
+    const request = {
+      cohorts: this.cohorts,
+      modality: this.modality,
+    };
+    const sub = this.apiService.fetchChordsData(request).subscribe({
+      next: (v) => {
+        // Adjust the maximum value of the slider
+        if (v['nodes'].length > 100) {
+          this.totalFeatures = 100;
+        } else {
+          // Round up totalFeatures for smooth scaling in the slider
+          this.totalFeatures = Math.max(
+            this.minFeatures,
+            Math.ceil(v['nodes'].length / 10) * 10
+          );
+        }
+        // Chunk the data based on the user adjusted value of the slider
+        this.dataChunks = this.chordService.chunkData(v, this.maxFeatures);
+        // Return no data message if all the chunks are empty
+        this.noData = this.dataChunks.every(
+          (chunk) => chunk.nodes.length === 0
+        );
+        if (!this.noData) {
+          this.chordService.createChordDiagrams(this.dataChunks);
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching chord data', err);
+        this.loading = false;
+        const detail = err.error?.detail;
+        const message = err.error?.message || err.message;
+
+        let errorMessage = 'An unknown error occurred.';
+        if (detail && message) {
+          errorMessage = `${message} — ${detail}`;
+        } else if (detail || message) {
+          errorMessage = detail || message;
+        }
+
+        alert(`An error occurred while fetching chord data: ${errorMessage}`);
+      },
+      complete: () => (this.loading = false),
+    });
+    this.subscriptions.push(sub);
+  }
+
+  fetchModalities(): void {
+    this.loading = true;
+    const sub = this.apiService.fetchModalities().subscribe({
+      next: (v) => {
+        this.modalities = v;
+      },
+      error: (err) => {
+        console.error('Error fetching modalities', err);
+        this.loading = false;
+        const detail = err.error?.detail;
+        const message = err.error?.message || err.message;
+
+        let errorMessage = 'An unknown error occurred.';
+        if (detail && message) {
+          errorMessage = `${message} — ${detail}`;
+        } else if (detail || message) {
+          errorMessage = detail || message;
+        }
+
+        alert(`An error occurred while fetching modalities: ${errorMessage}`);
+      },
+      complete: () => (this.loading = false),
+    });
+    this.subscriptions.push(sub);
+  }
 
   ngOnInit(): void {
     this.fetchModalities();
@@ -84,78 +182,5 @@ export class MappingsComponent implements OnInit, OnDestroy {
       /\w\S*/g,
       (txt) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase()
     );
-  }
-
-  private fetchCohorts(): void {
-    this.loading = true;
-    const sub = this.http
-      .get<string[]>(`${this.API_URL}/cdm/cohorts`)
-      .subscribe({
-        next: (v) => {
-          this.cohorts = v;
-        },
-        error: (e) => {
-          this.loading = false;
-          console.error('Error fetching cohorts:', e);
-        },
-        complete: () => (this.loading = false),
-      });
-    this.subscriptions.push(sub);
-  }
-
-  private fetchData(): void {
-    this.loading = true;
-    const request = {
-      cohorts: this.cohorts,
-      modality: this.modality,
-    };
-    const sub = this.http
-      .post<ChordData>(`${this.API_URL}/visualization/chords/`, request)
-      .subscribe({
-        next: (v) => {
-          // Adjust the maximum value of the slider
-          if (v['nodes'].length > 100) {
-            this.totalFeatures = 100;
-          } else {
-            // Round up totalFeatures for smooth scaling in the slider
-            this.totalFeatures = Math.max(
-              this.minFeatures,
-              Math.ceil(v['nodes'].length / 10) * 10
-            );
-          }
-          // Chunk the data based on the user adjusted value of the slider
-          this.dataChunks = this.chordService.chunkData(v, this.maxFeatures);
-          // Return no data message if all the chunks are empty
-          this.noData = this.dataChunks.every(
-            (chunk) => chunk.nodes.length === 0
-          );
-          if (!this.noData) {
-            this.chordService.createChordDiagrams(this.dataChunks);
-          }
-        },
-        error: (e) => {
-          this.loading = false;
-          console.error('Error fetching chord data:', e);
-        },
-        complete: () => (this.loading = false),
-      });
-    this.subscriptions.push(sub);
-  }
-
-  private fetchModalities(): void {
-    this.loading = true;
-    const sub = this.http
-      .get<string[]>(`${this.API_URL}/cdm/modalities`)
-      .subscribe({
-        next: (v) => {
-          this.modalities = v;
-        },
-        error: (e) => {
-          this.loading = false;
-          console.error('Error fetching the modalities:', e);
-        },
-        complete: () => (this.loading = false),
-      });
-    this.subscriptions.push(sub);
   }
 }
