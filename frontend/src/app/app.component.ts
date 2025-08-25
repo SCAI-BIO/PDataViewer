41d674b237ae4d84b72eee6fc6c55ebf0ebbb38f
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-25 10:45:42 +0200

    fix: correct styleUrl property in app component decorator

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,9 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, FooterComponent, NavBarComponent],
-    templateUrl: './app.component.html'
+  selector: 'app-root',
+  imports: [RouterOutlet, FooterComponent, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pdataviewer';
