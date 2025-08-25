8b48a693819a3ab2d11b780e03ea7a4aa0b19f39
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-25 11:20:48 +0200

    feat: add ApiService with methods for fetching biomarker data, cohorts, and metadata

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { Metadata } from '../interfaces/metadata';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private readonly API_URL = environment.API_URL;
+  private http = inject(HttpClient);
+
+  fetchBiomarkerData(
+    biomarker: string,
+    cohort: string,
+    diagnosis: string
+  ): Observable<number[]> {
+    return this.http.get<number[]>(
+      `${this.API_URL}/biomarkers/${biomarker}/cohorts/${cohort}/diagnoses/${diagnosis}`
+    );
+  }
+
+  fetchBiomarkers(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.API_URL}/biomarkers`);
+  }
+
+  fetchCohortsForBiomarker(biomarker: string): Observable<string[]> {
+    return this.http.get<string[]>(
+      `${this.API_URL}/biomarkers/${biomarker}/cohorts`
+    );
+  }
+
+  fetchColors(): Observable<Metadata> {
+    return this.http.get<Metadata>(`${this.API_URL}/cohorts/metadata`);
+  }
+
+  fetchDiagnosesForBiomarker(
+    biomarker: string
+  ): Observable<Record<string, string[]>> {
+    return this.http.get<Record<string, string[]>>(
+      `${this.API_URL}/biomarkers/${biomarker}/diagnoses`
+    );
+  }
+}
