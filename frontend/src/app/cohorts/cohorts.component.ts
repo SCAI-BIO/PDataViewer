56191acb6efc892b1f59b25b4fefbf63012fa4b6
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-25 10:46:06 +0200

    refactor: update cohort component structure and styles for consistency

--- frontend/src/app/cohorts/cohorts.component.ts
+++ frontend/src/app/cohorts/cohorts.component.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy, ViewChild, inject } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSortModule, MatSort, Sort } from '@angular/material/sort';
 
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 import { Metadata, CohortMetadata } from '../interfaces/metadata';
 import { environment } from '../../environments/environment';
@@ -14,52 +14,52 @@ interface CohortData extends CohortMetadata {
 }
 @Component({
   selector: 'app-cohorts',
-  imports: [MatTableModule, MatSortModule],
+  imports: [MatProgressSpinnerModule, MatSortModule, MatTableModule],
   templateUrl: './cohorts.component.html',
   styleUrl: './cohorts.component.scss',
 })
 export class CohortsComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = [
     'cohort',
-    'Participants',
-    'HealthyControls',
-    'ProdromalPatients',
-    'PDPatients',
-    'LongitudinalPatients',
-    'FollowUpInterval',
-    'Location',
-    'DOI',
-    'Link',
+    'participants',
+    'healthyControls',
+    'prodromalPatients',
+    'pdPatients',
+    'longitudinalPatients',
+    'followUpInterval',
+    'location',
+    'doi',
+    'link',
   ];
   dataSource = new MatTableDataSource<CohortData>();
+  loading = false;
   @ViewChild(MatSort) sort!: MatSort;
   private API_URL = environment.API_URL;
   private destroy$ = new Subject<void>();
   private http = inject(HttpClient);
 
   fetchMetadata(): void {
-    this.http
-      .get<Metadata>(`${this.API_URL}/cohorts/metadata`)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (data) => {
-          const transformedData = Object.keys(data).map((key) => ({
-            cohort: key,
-            ...data[key],
-          }));
-          this.dataSource.data = transformedData;
-          this.dataSource.sort = this.sort;
+    this.loading = true;
+    this.http.get<Metadata>(`${this.API_URL}/cohorts/metadata`).subscribe({
+      next: (data) => {
+        const transformedData = Object.keys(data).map((key) => ({
+          cohort: key,
+          ...data[key],
+        }));
+        this.dataSource.data = transformedData;
+        this.dataSource.sort = this.sort;
 
-          const initialSortState: Sort = { active: 'cohort', direction: 'asc' };
-          this.sort.active = initialSortState.active;
-          this.sort.direction = initialSortState.direction;
-          this.sort.sortChange.emit(initialSortState);
-        },
-        error: (e) => {
-          console.error('Error fetching metadata', e);
-        },
-        complete: () => console.info('Metadata successfully fetched'),
-      });
+        const initialSortState: Sort = { active: 'cohort', direction: 'asc' };
+        this.sort.active = initialSortState.active;
+        this.sort.direction = initialSortState.direction;
+        this.sort.sortChange.emit(initialSortState);
+      },
+      error: (e) => {
+        this.loading = false;
+        console.error('Error fetching metadata', e);
+      },
+      complete: () => (this.loading = false),
+    });
   }
 
   openLink(link: string) {
