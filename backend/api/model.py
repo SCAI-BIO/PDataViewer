e15074362397b478f4f62a7b2e72bb8c4f95d9c8
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-09-09 14:52:36 +0200

    fix: remove unused ChordsRequest model to streamline code

--- backend/api/model.py
+++ backend/api/model.py
@@ -4,17 +4,11 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class ChordsRequest(BaseModel):
-    modality: str
-    cohorts: list[str]
-
-
 class PathModel(BaseModel):
     path: str
 
 
 class CohortMetadata(BaseModel):
-    name: str
     participants: Optional[int]
     controlParticipants: Optional[int]
     prodromalParticipants: Optional[int]
