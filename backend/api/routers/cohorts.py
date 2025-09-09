a95fd74cec2f592ea37d582e77ce2369e13abb15
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-09-09 13:51:09 +0200

    fix: refactor cohort metadata retrieval to use response model for improved clarity

--- backend/api/routers/cohorts.py
+++ backend/api/routers/cohorts.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 
+from database.postgresql import PostgreSQLRepository
 from fastapi import APIRouter, Depends
 
 from api.dependencies import get_client
-from backend.database.postgresql import PostgreSQLRepository
+from api.model import CohortMetadata
 
 router = APIRouter(prefix="/cohorts", tags=["cohorts"], dependencies=[Depends(get_client)])
 
@@ -14,6 +15,23 @@ def get_cohorts(database: Annotated[PostgreSQLRepository, Depends(get_client)]):
     return [cohort.name for cohort in cohorts]
 
 
-@router.get("/metadata", description="Get cohort metadata")
+@router.get("/metadata", response_model=list[CohortMetadata], description="Get cohort metadata")
 def get_metadata(database: Annotated[PostgreSQLRepository, Depends(get_client)]):
-    return database.get_cohorts()
+    cohort_metadata = database.get_cohorts()
+
+    return [
+        CohortMetadata(
+            name=cm.name,
+            participants=cm.participants,
+            controlParticipants=cm.control_participants,
+            prodromalParticipants=cm.prodromal_participants,
+            pdParticipants=cm.pd_participants,
+            longitudinalParticipants=cm.longitudinal_participants,
+            followUpInterval=cm.follow_up_interval,
+            location=cm.location,
+            doi=cm.doi,
+            link=cm.link,
+            color=cm.color,
+        )
+        for cm in cohort_metadata
+    ]
