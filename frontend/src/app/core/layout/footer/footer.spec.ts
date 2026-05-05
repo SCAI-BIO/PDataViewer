bf0a4bd5ad1136320a6d8106993fa7f22f62b1e0
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-05-05 16:13:50 +0200

    fix: broken tests

--- frontend/src/app/core/layout/footer/footer.spec.ts
+++ frontend/src/app/core/layout/footer/footer.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { Footer } from './footer';
 
@@ -9,6 +10,7 @@ describe('Footer', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Footer],
+      providers: [provideRouter([])],
       teardown: { destroyAfterEach: false },
     }).compileComponents();
   });
